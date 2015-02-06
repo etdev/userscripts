67a80d39f63cd8897f9fcc5574d9f0c6d8117427
Eric Turner <ericturnerdev@gmail.com>
2015-02-06 16:52:31 +0900

    Update reddit.user.js

--- scripts/reddit.user.js
+++ scripts/reddit.user.js
@@ -4,26 +4,40 @@
 // @version      0.1
 // @description  Turn off sidebar by default and add toggle to turn it back on
 // @author       etdev (etdev.me)
-// @match        /reddit.com.*\/r\/.*comments.*/
+// @match        https://www.reddit.com/* 
 // @grant        none
 // ==/UserScript==
+
 window.onload = function() {
+  turnOffSubredditStyle();
   toggleSidebar();
 };
 
 function toggleSidebar() {
-  var unwanted = [ ".linkinfo", ".sponsorshipbox", ".sidebox.submit.submit-link", ".sidebox.submit.submit-text", ".titlebox", ".sidecontentbox", ".account-activity-box" ];
+  var unwanted = [ ".linkinfo", ".sponsorshipbox", ".sidebox.submit.submit-link", ".sidebox.submit.submit-text", ".titlebox", ".sidecontentbox", ".account-activity-box", ".goldvertisement" ];
   for (var i=0; i<unwanted.length; i++) {
     toggleVis(unwanted[i]);
   }
 }
 
 function toggleVis(selector) {
-  console.log("selector: " + selector);
   var results = document.querySelectorAll(selector);
-  for (var i=0; i<results.length; i++) {
-    result = results[i];
-    if (result.style.display === "none") { result.style.display = "visible"; }
-    else { result.style.display = "none"; }
+  if (results) {
+    for (var i=0; i<results.length; i++) {
+      result = results[i];
+      if (result.style.display === "none") { result.style.display = "visible"; }
+      else { result.style.display = "none"; }
+    }
+  }
+}
+
+function turnOffSubredditStyle() {
+  var checkbox = null;
+  try {
+    checkbox = document.querySelector(".titlebox [type=checkbox]");
+  } catch(e) { }
+
+  if (checkbox) {
+    checkbox.checked = false;
   }
 }
